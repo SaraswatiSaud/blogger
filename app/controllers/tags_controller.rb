71f47efe04fe58ae97bd740f5c827cea9c2359a0
SaraswatiSaud <saraswati.saud@gmail.com>
2017-02-16 07:40:02 +0545

    added a few gems

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,28 @@
 class TagsController < ApplicationController
 
+  def index
+    @tag = Tag.all
+  end
+
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
   end
 
 end
